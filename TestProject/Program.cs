a5bcb1c1192734cc4c72ccc252278914bf3ded5f
John W. Stokes, Jr. <tkdjohn71@gmail.com>
2025-10-29 00:18:17 -0400

    M1W1 and M1W2

---
+++ TestProject/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, Joe!");
+        Console.Write("my first line of code> \f >> ");
+        Console.WriteLine("look I'm on the same line.");
+        var five = 5;
+        Console.WriteLine(five);
+        Console.WriteLine(five++);
+        Console.WriteLine(five);
+        Console.WriteLine(++five);
+        Console.WriteLine(five);
+
+        Console.WriteLine("================");
+        Console.Write("Enter your name: ");
+        var name = Console.ReadLine();
+        Console.WriteLine($"Hello, {name}!");
+    }
+}
